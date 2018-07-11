4a2fd464306dcf57d32f1907a9665a684a922243
Patricia Carter <patricia4194@gmail.com>
2018-07-11 19:38:16 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 var recipes = {eggs : 3, milk : 1, cheese : 2}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  return Object.assign(object, { [key] : value})
+  return Object.assign({}, obj, { [key] : value})
 }
 
 updateObjectWithKeyAndValue(recipes, 'butter', 4)
