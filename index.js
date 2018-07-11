4567110f7ef24daad2d6e59015514994895858ba
Patricia Carter <patricia4194@gmail.com>
2018-07-11 19:37:30 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -4,6 +4,6 @@ function updateObjectWithKeyAndValue(object, key, value) {
   return Object.assign(object, { [key] : value})
 }
 
-updateObjectWithKeyAndValue(recipes, butter, 4)
+updateObjectWithKeyAndValue(recipes, 'butter', 4)
 
 console.log(recipes)
