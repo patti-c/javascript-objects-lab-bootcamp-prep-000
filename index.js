8032e6413d3386d9a313b8604d62159acc46d6d1
Patricia Carter <patricia4194@gmail.com>
2018-07-11 19:31:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var recipes = {eggs : 3, milk : 1, cheese : 2}
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  object = [...object, {key: value}]
+  object = [...object, {[key]: [value]}]
   return object
 }
